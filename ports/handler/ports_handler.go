d401ed5697aa5749ed57f197078ca0ceb8640d99
Nihanth Dara <nihanthd@gmail.com>
2018-12-17 09:33:52 -0600

    Added Toggle functionality. Committing missed files due to bad git ignore.

--- ports/handler/ports_handler.go
+++ ports/handler/ports_handler.go
@@ -16,6 +16,10 @@ func NewPortsHandler(pi *pi.PI) *PortsHandler {
 	return p
 }
 
+func (p *PortsHandler) TogglePort(c echo.Context) error {
+	return p.PI.Turn(c)
+}
+
 func (p *PortsHandler) GetPortStatus(c echo.Context) error {
 	return p.PI.Status(c)
 }
